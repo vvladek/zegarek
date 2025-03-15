f08ef3e6ab3207a78c0389b4e99bc1bac23cf087
Wladek <wladek.developer@gmail.com>
2025-03-15 15:46:16 +0100

    feat: add background changing script

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { ImageBackground, StyleSheet, View, Dimensions, Text } from "react-nativ
 import { StatusBar } from "expo-status-bar";
 import { BlurView } from "expo-blur";
 import { useEffect, useState } from "react";
+import { backgrounds } from "@/constants/backgrounds";
 
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
@@ -10,6 +11,7 @@ const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 export default function HomeScreen() {
 
   const [time, setTime] = useState<string>("10:10");
+  const [bgImg, setBgImg] = useState(backgrounds.sleeping);
 
   function normalizeTime(time: number) {
     return `${time < 10 ? "0" : ""}${time}`
@@ -23,6 +25,10 @@ export default function HomeScreen() {
       const hours = date.getHours();
       const minutes = date.getMinutes();
       setTime(`${normalizeTime(hours)}:${normalizeTime(minutes)}`);
+      if (hours > 18 && hours < 6) setBgImg(backgrounds.sleeping);
+      else if (hours >= 6 && hours < 7) setBgImg(backgrounds.closedDoors);
+      else if (hours >= 7 && hours < 9) setBgImg(backgrounds.openDoors);
+      else setBgImg(backgrounds.playing);
     }, 1000);
 
     return () => clearInterval(timer);
@@ -31,7 +37,7 @@ export default function HomeScreen() {
 
   return (
     <ImageBackground
-      source={require("@/assets/images/sleeping.webp")}
+      source={bgImg}
       style={styles.imageBackground}
     >
       <StatusBar translucent={true} style="auto" />
---
+++ constants/backgrounds.ts
@@ -0,0 +1,8 @@
+
+
+export const backgrounds = {
+  sleeping: require("@/assets/images/sleeping.webp"),
+  closedDoors: require("@/assets/images/closed-doors.webp"),
+  openDoors: require("@/assets/images/open-doors.webp"),
+  playing: require("@/assets/images/playing.webp"),
+};
